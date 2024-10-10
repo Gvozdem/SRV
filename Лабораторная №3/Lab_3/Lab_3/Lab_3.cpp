1df3121bc961b7f7910e9a8821e3028e0f1e1eac
Gvozdem <gvozdenko.demid@mail.ru>
2024-10-10 21:00:03 +0200

    Added 2.3.4 labs

---
+++ Лабораторная №3/Lab_3/Lab_3/Lab_3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    clock_t start, end;
+    start = clock();
+    std::string message = "the program is still alive!";
+    char exitKey = 'q';
+    char changeMessageKey = 'c';
+
+    std::cout << "Press any key to start\n";
+    char x = _getch();
+    std::cout << "start - " << x << std::endl;
+
+    while (!_kbhit())
+    {
+        end = clock();
+        if ((double)(end - start) / CLOCKS_PER_SEC >= 1)
+        {
+            std::cout << message << std::endl;
+            start = clock();
+        }
+
+        if (_kbhit() && _getch() == changeMessageKey)
+        {
+            message = "The message has been changed!";
+        }
+
+        if (_kbhit() && _getch() == exitKey)
+        {
+            std::cout << "Exiting the program..." << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
