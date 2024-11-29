28dc2c6e1c81a1ac44d56c9232a25a87696166ac
Gvozdem <gvozdenko.demid@mail.ru>
2024-11-29 23:37:24 +0200

    Added 9 lab

---
+++ Lab_9/Lab_9/Lab_9.cpp
@@ -0,0 +1,146 @@
+﻿#include <iostream>
+#include <WS2tcpip.h>
+#include <winsock2.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+//Код из примера
+
+//void main()
+//{
+//    WSADATA data;
+//    WORD version = MAKEWORD(2, 2);
+//
+//    int wsOk = WSAStartup(version, &data);
+//    if (wsOk != 0)
+//    {
+//        cout << "Can't start Winsock" << wsOk;
+//        return;
+//    }
+//
+//    SOCKET in = socket(AF_INET, SOCK_DGRAM, 0);
+//
+//    sockaddr_in serverHint;
+//    serverHint.sin_addr.S_un.S_addr = ADDR_ANY;
+//    serverHint.sin_family = AF_INET;
+//
+//    serverHint.sin_port = htons(54000);
+//
+//    if (bind(in, (sockaddr*)&serverHint, sizeof(serverHint)) == SOCKET_ERROR)
+//    {
+//        cout << "Can't bind socket!" << WSAGetLastError() << endl;
+//        return;
+//    }
+//
+//    sockaddr_in client;
+//    int clientLength = sizeof(client);
+//    char buf[1024];
+//
+//    while (true)
+//    {
+//        ZeroMemory(&client, clientLength);
+//        ZeroMemory(buf, 1024);
+//
+//        int bytesIn = recvfrom(in, buf, 1024, 0, (sockaddr*)&client, &clientLength);
+//        if (bytesIn == SOCKET_ERROR)
+//        {
+//            cout << "Error receiving from client" << WSAGetLastError() << endl;
+//            continue;
+//        }
+//        char clientIp[256];
+//        ZeroMemory(clientIp, 256);
+//
+//        inet_ntop(AF_INET, &client.sin_addr, clientIp, 256);
+//
+//        cout << "Message recv from" << clientIp << " : " << buf << endl;
+//    }
+//    closesocket(in);
+//    WSACleanup();
+//}
+
+
+
+//Код для TSP
+void main()
+{
+	std::string EXIT_MESSAGE = "EXIT";
+	WSADATA data;
+	WORD version = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(version, &data);
+	if (wsOk != 0)
+	{
+		cout << "Can't start Winsock" << wsOk;
+		return;
+	}
+
+	SOCKET in = socket(AF_INET, SOCK_STREAM, 0);
+
+	sockaddr_in serverHint;
+	serverHint.sin_addr.s_addr = INADDR_ANY;
+	serverHint.sin_family = AF_INET;
+	serverHint.sin_port = htons(54000);
+
+	if (bind(in, (sockaddr*)&serverHint, sizeof(serverHint)) == SOCKET_ERROR)
+	{
+		cout << "Can't bind socket!" << WSAGetLastError << endl;
+		return;
+	}
+
+	listen(in, SOMAXCONN);
+
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+	SOCKET clientSocket = accept(in, (sockaddr*)&client, &clientSize);
+
+	char host[NI_MAXHOST];
+	char service[NI_MAXSERV];
+
+	ZeroMemory(host, NI_MAXHOST);
+	ZeroMemory(service, NI_MAXSERV);
+
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		cout << host << " connected on port " << service << endl;
+	}
+	else
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+	}
+
+	closesocket(in);
+
+	char buf[4096];
+	while (true)
+	{
+		ZeroMemory(buf, 4096);
+
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			cout << "Error in recv(). Quitting" << endl;
+			break;
+		}
+
+		if (bytesReceived == 0)
+		{
+			cout << "Client disconnected" << endl;
+			break;
+		}
+
+		if (strncmp(buf, EXIT_MESSAGE.c_str(), EXIT_MESSAGE.length()) == 0)
+		{
+			cout << "Получено сообщение 'EXIT'. Завершаю работу сервера." << endl;
+
+			break;
+		}
+
+		send(clientSocket, buf, bytesReceived + 1, 0);
+	}
+
+	closesocket(clientSocket);
+	WSACleanup();
+}
---
+++ test_client/test_client/test_client.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+
+#pragma comment (lib, "ws2_32.lib")
+
+using namespace std;
+
+//Из примера
+
+//void main()
+//{
+//	WSADATA data;
+//	int wsOk = WSAStartup(MAKEWORD(2, 2), &data);
+//	if (wsOk != 0)
+//	{
+//		cout << "Can't start Winsock!" << wsOk;
+//	}
+//
+//	sockaddr_in server;
+//	server.sin_family = AF_INET;
+//	server.sin_port = htons(54000);
+//
+//	inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+//
+//	SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
+//
+//	string msg = "";
+//	while (true)
+//	{
+//		msg.clear();
+//		cin >> msg;
+//
+//		int sendOk = sendto(out, msg.c_str(), msg.size() + 1, 0, (sockaddr*)&server, sizeof(server));
+//		if (sendOk == SOCKET_ERROR)
+//		{
+//			cout << "That didn't work!" << WSAGetLastError() << endl;
+//		}
+//	}
+//	closesocket(out);
+//	WSACleanup();
+//}
+
+
+//Клиент для TCP сервера
+void main() {
+    WSADATA wsaData;
+    int wsOk = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (wsOk != 0)
+    {
+        cout << "Can't start Winsock!" << wsOk;
+        return;
+    }
+
+    sockaddr_in serverHint;
+    serverHint.sin_family = AF_INET;
+    serverHint.sin_port = htons(54000);
+    inet_pton(AF_INET, "127.0.0.1", &serverHint.sin_addr);
+
+    SOCKET out = socket(AF_INET, SOCK_STREAM, 0);
+
+    int connResult = connect(out, (sockaddr*)&serverHint, sizeof(serverHint));
+    if (connResult == SOCKET_ERROR)
+    {
+        cout << "Can't connect to server, ERROR #" << WSAGetLastError() << endl;
+        closesocket(out);
+        WSACleanup();
+        return;
+    }
+
+    char buf[4096];
+    string userInput;
+
+    do
+    {
+        cout << "> ";
+        getline(cin, userInput);
+
+        if (userInput.size() > 0)
+        {
+            int sendResult = send(out, userInput.c_str(), userInput.size() + 1, 0);
+            if (sendResult != SOCKET_ERROR)
+            {
+                ZeroMemory(buf, 4096);
+                int bytesReceived = recv(out, buf, 4096, 0);
+                if (bytesReceived > 0)
+                {
+                    cout << "SERVER> " << string(buf, 0, bytesReceived) << endl;
+                }
+            }
+        }
+    } while (userInput.size() > 0);
+
+    closesocket(out);
+    WSACleanup();
+}
